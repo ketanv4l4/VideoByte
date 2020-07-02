4326f4faec91aebdfac7e75d811ed17e439410dd
ketanvala7 <ketanvala2002@gmail.com>
2020-07-02 22:33:57 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={`${process.env.PUBLIC_URL}/`}>
           <Search />
           <Switch>
             <Route exact path="/" component={Home} />
